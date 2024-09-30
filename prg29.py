1bd9acc02d518a1e01b5872be6fb8780ee4f634b
student <9651027@mail.ru>
2024-09-30 11:11:33 +0300

    string method

--- prg29.py
+++ prg29.py
@@ -1,13 +1,15 @@
 # Основные методы строки
 
-s = 'ЯзыК программирования Python'
+s = 'Телевидение'
 
-print(s.lower()) # Все буквы маленькие
-print(s.upper()) # Все буквы большие
-print(s.cspitalize()) # First letter first word
-print(s.title()) # Allfirdt letters in word - CAPS
-print(s.strip()) # удаляет символы заключенные в скобках слева и справат со строки перед и после пробелы - без аргументов - пробел
-print(s.strip()) # снимает со строки перед и после пробелы - без аргументов
-print(s.lstrip()) # l - left r- rights
-print(s.lstrip("n").lower()) # цепочка вызовов
-print(s.count('е', 4))
+# print(s.lower()) # Все буквы маленькие
+# print(s.upper()) # Все буквы большие
+# print(s.cspitalize()) # First letter first word
+# print(s.title()) # Allfirdt letters in word - CAPS
+# print(s.strip()) # удаляет символы заключенные в скобках слева и справат со строки перед и после пробелы - без аргументов - пробел
+# print(s.strip()) # снимает со строки перед и после пробелы - без аргументов
+# print(s.lstrip()) # l - left r- rights
+# print(s.lstrip("n").lower()) # цепочка вызовов
+# print(s.count('е', 4))
+
+print(s.index('ви'))  # индекс по символу
