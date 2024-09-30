771c23a614903b666a165ccfdf87ad6f6eab91e1
student <9651027@mail.ru>
2024-09-30 12:04:01 +0300

    chr_ord

---
+++ prg31.py
@@ -0,0 +1,9 @@
+  # Функции антонимы chr (символ) и ord (порядковый номер)
+  # ASCII (256)
+  # UNICODE (65536)
+
+print(ord('a'))
+print(chr(97))
+
+s = "Hello"
+print(f'{chr(171)}Hello{chr(187)}')
