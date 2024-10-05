33ce0bfbd6a437b319c1ef4f245be56eb64c7291
vanya2001 <ivangurianov2017@gmail.com>
2024-10-05 07:00:18 +0300

    repeat lesson3 infinity loop keywords up_to_prg16_1

---
+++ !_keywords_python.py
@@ -0,0 +1,15 @@
+# import keyword
+# keyword.kwlist
+['False', 'None', 'True', 'and', 'as', 'assert', 'async',
+ 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global',
+ 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with',
+ 'yield']
+
+"""
+input(prompt='', /)
+    Read a string from standard input.  The trailing newline is stripped.
+    The prompt string, if given, is printed to standard output without a
+    trailing newline before reading input.
+    If the user hits EOF (*nix: Ctrl-D, Windows: Ctrl-Z+Return), raise EOFError.
+    On *nix systems, readline is used if available.
+"""
