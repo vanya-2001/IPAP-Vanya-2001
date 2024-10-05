889ece98cf7087458c29e691ae27f2d119bd436e
vanya2001 <ivangurianov2017@gmail.com>
2024-10-05 17:11:50 +0300

    repeat lesson3 "for" itarable object intro up_to_prg18_1

---
+++ prg18_1.py
@@ -0,0 +1,23 @@
+# Итерируемый объект (itarable object)
+#  и первые операции с ним.
+# int и float не являются итерируемыми  объектами
+# итерирунмый объект стоит справа от in
+
+"""
+string = 'абвгдейка'
+
+for symbol in string:
+    print(symbol)
+"""
+"""
+string = 'росток'
+if 'ток' in string:
+    print('ток есть в строке', string)
+"""
+
+"""
+string = 'Вася Пупкин'
+if 'Пупкин' in string:
+    print('Не надо писать', '"', string, '"')
+"""
+
