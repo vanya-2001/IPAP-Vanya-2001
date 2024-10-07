9fa056f4ff2d2c2efe5c3209191d788b3708e5af
vanya2001 <ivangurianov2017@gmail.com>
2024-10-07 11:09:06 +0300

    lesson6 list intro inpack  up_to_prg32_

--- prg32.py
+++ prg32.py
@@ -5,42 +5,23 @@ empty_set = set()
 array = []  # пустой список
 array = list()  # пустой список
 # index     0         1        2
-array = ['ясно', 'пасмурно', '36.6']
-
-# print(array[2][3])
-# print(array[::2])
-
+array = ['ясно', 'пасмурно', 36.6]
 okroshka = ['огурец', 'квас', 'кефир', 'соль', 'перец']
-#
-# print(okroshka[0:len(okroshka):2])  # [::2]
-# print(okroshka[-4::-1])
 
-word = 'pitton'  # immutable
+print(okroshka[0:len(okroshka):2])  # [::2]
+
+word = 'pithon'  # immutable
 # word[1] = 'y' - так нельзя
 lst = list(word)
 
-# print(lst)
+print(lst)
 
 lst[1] = 'y'
-lst[2] = ''
-print(''.join(lst))
-
-# wlst = ''.join(lst)
-# print(wlst)
-
 
 print(*lst, sep='')
 
-
 dishes = ['борщ', 'пюре', 'компот', 'сок']
-first, *sec, last = dishes
-print(sec)
 # first, second, third = dishes
-# first, second, *drink = dishes
-# *eat, compot, drink = dishes
-# print(drink)
-# sup = ''.join(dishes[0])
-# print(sup)
-for i in range(len(dishes)):
-    print(''.join(dishes[i]))
+first, second, *drink = dishes
 
+print(drink)
