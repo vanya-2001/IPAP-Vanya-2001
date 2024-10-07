595976c494bedfc35c92345c23ed567196412822
vanya2001 <ivangurianov2017@gmail.com>
2024-10-07 07:25:19 +0300

    repeat lesson6 string format and set  up_to_prg31_1

--- prg31_1.py
+++ prg31_1.py
@@ -1,9 +1,22 @@
 # Функции chr ord
 # ASCII (256)
 # INICOD (65536)
-print(ord('a'))
+# """
+# print(ord('a'))
+#
+# print(chr(171))
+# print(chr(181))
+# print(f'{chr(171)}Привет!{chr(187)}')
+# # \x означает что мы хотим напечатать символ в 16-чной кодировке ASCII
+# # \xB0 = chr(176)
+# print('75' + chr(176) + 'C')
+# print(f'75{chr(176)}C')
+# print('75\xB0C') # hex ASCII
+# print('\u2710') # uniCode
+# """
 
-print(chr(171))
-print(chr(181))
+import string as s
 
-print(f'{chr(171)}Привет!{chr(187)}')
+temp = s.digits + s.ascii_lowercase + s.ascii_uppercase
+m_set = set(temp) - {'l', 'O', 'I', '1', '0'}
+print(m_set)
