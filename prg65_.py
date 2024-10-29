c894138dad7fa0311e47864a357b188e8664a02f
Vanya2001 <911v1997972@gmail.com>
2024-10-29 06:57:36 +0300

    OP Magic Methods up_to_65

--- prg65_.py
+++ prg65_.py
@@ -20,11 +20,24 @@ class Square:
 
     def __lt__(self, other):
         return self.side < other.side
+    def __sub__(self, other):
+        return Square(self.side - other.side)
+
+    def __repr__(self):
+        return f'Квадрат со стороной {self.side}'
+
+    def __del__(self):
+        print(f'Квадрат со стороной {self.side} стёрт!')
 
 
 
 sq1 = Square(5)
 sq2 = Square(4)
+
+sq1 = Square(10)
+sq2 = Square(6)
+complex_sq = [Square(2), Square(3)]
+
 print(sq1 == sq2) # False Адреса в памяти не равны
 print(sq1 > sq2)
 print(sq1 < sq2)
