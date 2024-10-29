44a344b4cac2b879258fd07918f309f4226f256e
Vanya2001 <911v1997972@gmail.com>
2024-10-29 18:46:51 +0300

    PIL Image Operations up_to_68

--- prg68_.py
+++ prg68_.py
@@ -1,31 +1,13 @@
 # Доп. библиотека PIL (pillow)
-from PIL import Image, ImageFilter
+from PIL import ImageDraw, Image
 
-# пробуем прочитать файл original.jpg
-try:
-    orig = Image.open('original.jpg')
-except FileNotFoundError:
-    print('Файл не найден')
+# создаем и рисуем original.jpg
+# холст
+canvas = Image.new("RGB", (200, 200), (255, 255, 255))
 
-print('Параметры считано файла:')
-print(f'Формат: {orig.format}')
-print(f'Размеры: {orig.size}')
-print(f'Цветовая схема: {orig.mode}')
+# создаём рисовальщик
+draw = ImageDraw.Draw(canvas)
 
-w, h = orig.size  # получили и распаковали кортеж
-pixels = orig.load()  # список пикселей [x, y]
+draw.line((0, 0, 200,200), fill=(244,0,0), width=3)
 
-for x in range(w):
-    for y in range(h):
-        r, g, b = pixels[x, y]
-        average = (r + g + b) // 3
-        pixels[x, y] = average, average, average
-
-orig.save('grayscale.jpg')
-
-blur = orig.filter(ImageFilter.GaussianBlur(2))
-cropped = orig.crop((0, 0, 300, 400))
-contour = orig.filter(ImageFilter.CONTOUR)
-cropped.save('cropped.jpg')
-blur.save('blur.jpg')
-contour.save('contour.jpg')
+canvas.save('line.png', 'PNG')
