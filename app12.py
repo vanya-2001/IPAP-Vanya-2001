8a16964d3015aec8b62205216f3cba0a0d46563d
Vanya2001 <911v1997972@gmail.com>
2024-10-31 02:35:32 +0300

    widgets intro 1

---
+++ app12.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import (
+    QMainWindow, QApplication,
+    QLabel, QCheckBox, QComboBox,  QLineEdit,
+    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider
+)
+from PyQt6.QtCore import Qt
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("My App")
+        self.setFixedSize(QSize(600, 400))
+        #widget = QLabel("1")  # Создана метка с текстом 1.
+        #widget.setText("2")  # Создана метка с текстом 2.
+        widget = QLabel("Hello")
+        font = widget.font()
+        font.setPointSize(100)
+        widget.setFont(font)
+        widget.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+
+        self.setCentralWidget(widget)
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec()
