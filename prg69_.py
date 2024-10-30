a74048a2dc6a3cfd9dbca2bf51b63a2b65986aa5
Vanya2001 <911v1997972@gmail.com>
2024-10-30 14:50:40 +0300

    GUI App #1 & PyQt6 up_to_69

--- prg69_.py
+++ prg69_.py
@@ -1,25 +1,40 @@
 # GUI - Graphic User Interface
 # PyQt6 - на самостоятельное
 import tkinter
-
 from PIL import Image, ImageTk
 
-# корневой элемент приложения
-root = tkinter.Tk()
 
-frame = tkinter.Frame(root)
-frame.grid()
-# Добавляем ярлык
-label = tkinter.Label(frame, text='GUI').grid_configure(row=1, column=1)
-# Добавляем кнопку
-but = tkinter.Button(frame, text='Кнопка').grid_configure(row=2, column=1)
+class App():
+    def __init__(self):
+        # корневой элемент приложения
+        self.root = tkinter.Tk()
 
-# Добавим холст
-canvas = tkinter.Canvas(root, width=600, height=400)
+        self.frame = tkinter.Frame(self.root)
+        self.frame.grid()
+        # Добавляем ярлык
+        self.label = tkinter.Label(self.frame, text='Меняем изображение').grid_configure(row=1, column=1)
 
-# Добавляем изображение на холст
-image = Image.open('original.jpg')
-photo = ImageTk.PhotoImage(image)
-image = canvas.create_image(0, 0, anchor='nw', image=photo)
-canvas.grid_configure(row=2, column=2)
-root.mainloop()
+
+        # Добавляем кнопку
+        self.but = tkinter.Button(self.frame, text='Заменить', command=self.change).grid_configure(row=1, column=2)
+
+        # Добавим холст
+        self.canvas = tkinter.Canvas(self.root, width=600, height=400)
+
+        # Добавляем изображение на холст
+        self.image = Image.open('original.jpg')
+        self.photo = ImageTk.PhotoImage(self.image)
+        self.image = self.canvas.create_image(0, 0, anchor='nw', image=self.photo)
+        self.canvas.grid_configure(row=3, column=1)
+        self.root.mainloop()
+
+    # Метод change
+    def change(self):
+        print('Кнопка нажата')
+        self.image = Image.open('inverted.jpg')
+        self.photo = ImageTk.PhotoImage(self.image)
+        self.canvas.create_image(0, 0, anchor='nw', image=self.photo)
+        self.canvas.grid_configure(row=3, column=1)
+
+
+app = App()
